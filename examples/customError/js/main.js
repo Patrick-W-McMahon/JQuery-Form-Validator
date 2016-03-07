339a7911b9c3d2b77616a6422ac40d37c27c20d8
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-07 11:06:44 -0500

    Update main.js
    formatting

--- examples/customError/js/main.js
+++ examples/customError/js/main.js
@@ -1,16 +1,13 @@
 $( document ).ready(function() {
 
-
-
 	$("#testForm").validate({
 		onValidate:function(validator){
-				var myErrors = validator.getErrors();
-				for(var x=0;x<myErrors.length;x++){
-					var err = myErrors[x];
-					console.log("Error "+x+": Title: "+err.title+", Message: "+err.msg+".");
-				}
+			var myErrors = validator.getErrors();
+			for(var x=0;x<myErrors.length;x++){
+				var err = myErrors[x];
+				console.log("Error "+x+": Title: "+err.title+", Message: "+err.msg+".");
+			}
 		}
 	});
-	
 	
 });
