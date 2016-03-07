1eeea8051cc8beb3b558e9d6000d48944e2a7fec
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-07 11:05:29 -0500

    Added files via upload

---
+++ examples/customError/js/main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+
+
+	$("#testForm").validate({
+		onValidate:function(validator){
+				var myErrors = validator.getErrors();
+				for(var x=0;x<myErrors.length;x++){
+					var err = myErrors[x];
+					console.log("Error "+x+": Title: "+err.title+", Message: "+err.msg+".");
+				}
+		}
+	});
+	
+	
+});
