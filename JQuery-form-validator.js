deee00db848ae4876c67669d5b0fc645401d12d4
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-14 12:03:22 -0400

    Update JQuery-form-validator.js
    reformatted a lot of the code to hide private methods that should not be called by user code. Opened up more methods for customization of error events. Streamlined the customization using JQuery's extend() many values have been moved to the plugins options. New callable methods:
    onErrorsFound() is called when there is at least one error.
    onDisplayErrors() this is called after errors are found to display the errors.

--- JQuery-form-validator.js
+++ JQuery-form-validator.js
@@ -1,138 +1,154 @@
-(function ( $ ) {
-	$.fn.validate = function(args){
-		var self = this;
-		args = args || [];
-		var errorDisplay=undefined;
-		if(isObj(self.attr("errorDiplay"))){
-			errorDisplay = $(self.attr("error_diplay"));
-		}else if(isObj(args["error_display"])){
-			errorDisplay = $(args["error_display"]);
-		}
-		var errorMessages = [];
-		var event;
-		self.submit(function(e){
-			validate(e);
+;(function ( $, window, document, undefined ){
+	var pluginName = "validate";
+	var self;
+	var event;
+	var defaults={
+		error_display_id:"",
+		errorDisplayElm:"",
+		onValidate:function(){},
+		onErrorsFound:function(){},
+		onDisplayErrors:function(){
+			var ede = this.options.errorDisplayElm;
+			if(isObj(ede)){
+				var myErrors = this.getErrors()||[];
+				ede.empty();
+				for(var x=0;x<myErrors.length;x++){
+					ede.append($("<div><span class='title'>"+myErrors[x].title+"</span>: "+myErrors[x].msg+"</div>"));
+					ede.show();
+				}
+			}else{
+				console.log("error display element not set");
+			}
+		},
+	};
+	
+	$.fn[pluginName] = function(options){
+		return this.each(function(){
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+			}
 		});
-		
-		self.find('input[type=submit]').on('click',function(e){
-			validate(e);
-		});
-		
-		self.find('input[type=reset]').on('click',function(e){
-			reset(e);
-		});
-		
-		function reset(e){
-			event=e;
-			errorMessages = [];
-			errorDisplay.hide();
-		}
+	}
+	
+	//Plugin constructor
+	function Plugin(element,options){
+		self = this;
+		this.element = element;
+		this.options = $.extend({},defaults,options);
+		this._defaults = defaults;
+        this._name = pluginName;
+		this.errorMessages = [];
 		
 		this.err = function(title,msg){
-			errorMessages.push({
+			self.errorMessages.push({
 				"title":title,
 				"msg":msg
 			});
 		}
 		
-		function validate(e){
-			event=e;
-			errorMessages = [];
-			self.find(':invalid').each(function(index, node){
-				if($(this).is("input")){
-					self.err(getErrTitle($(this)),getErrMessage($(this)));
-				}
-			});
-			
-			self.find("[data-validate]").each(function(){
-				switch($(this).attr("data-validate").toLowerCase()){
-					case "checkboxgroup":
-					case "checkbox_group":
-					case "checkbox-group":
-						var count=0;
-						var checkboxes = $(this).find("input[type=checkbox]");
-						var passed=false;
-						$(this).find("input[type=checkbox]").each(function(){
-							if($(this).is(':checked')){
-								count++;
-							}
-						});
-						if($(this).attr("data-min-select")){
-							if(count<$(this).attr("data-min-select")){
-								self.err(getErrTitle($(this)),"min of "+$(this).attr("data-min-select")+" selections must be made");
-							}
-						}
-						if($(this).attr("data-max-select")){
-							if(count>$(this).attr("data-max-select")){
-								self.err(getErrTitle($(this)),"max of "+$(this).attr("data-max-select")+" selections can be made");
-							}
-						}
-						
-					break;
-				}
-			
-			});
-			var onVal = self.getArg('onValidate');
-			if(onVal){
-				onVal(self);
-			}
-			if(errorMessages.length>0){
-				stopSubmit();
-			}
-		}
-		
-		function getErrTitle(elm){
-			var label = elm.parent().find("label[for="+elm.attr("id")+"]");
-			if(isObj(label)&&label.html()!=undefined){
-				return label.html();
-			}
-			var elmAttrErr = elm.attr("data-err-title");
-			if(elmAttrErr.length>0){
-				return elmAttrErr;
-			}
-		}
-		
-		function getErrMessage(elm){
-			if(elm.attr("data-err-msg")){
-				return elm.attr("data-err-msg");
-			}
-			return "value required";
-		}
-		
 		this.getErrors = function(){
-			return errorMessages;
+			return self.errorMessages;
 		}
-		
-		function displayErrors(){
-			if(isObj(errorDisplay)){
-				errorDisplay.empty();
-				for(var x=0;x<errorMessages.length;x++){
-					errorDisplay.append($("<div><span class='title'>"+errorMessages[x].title+"</span>: "+errorMessages[x].msg+"</div>"));
-					errorDisplay.show();
-				}
-			}else{
-				console.log("error display element not set");
-			}
+		this.init();
+	}
+	
+	Plugin.prototype.init = function(){
+		if(isObj($(this.element).attr("errorDiplay"))){
+			this.options.errorDisplayElm = $($(this.element).attr("error_diplay"));
+		}else{
+			this.options.errorDisplayElm = $(this.options["error_display_id"]);
 		}
+		$(this.element).submit(function(e){
+			validate(e);
+		});
+		$(this.element).find('input[type=submit]').on('click',function(e){
+			validate(e);
+		});
 		
-		function stopSubmit(){
-			displayErrors();
-			if(typeof(event)!=="undefined"){
-				event.preventDefault();
-			}
-		}
-		
-		this.getArg = function(val){
-			if(isObj(args)){
-				if(isObj(args[val])&&args[val]){
-					return args[val];
-				}
-			}
-			return false;
-		}
-		
-		function isObj(t){
-			return typeof(t)!==undefined;
+		$(this.element).find('input[type=reset]').on('click',function(e){
+			reset(e);
+		});
+	}
+	
+	function stopSubmit(f){
+		f.options.onDisplayErrors.call(f);
+		if(typeof(event)!=="undefined"){
+			event.preventDefault();
 		}
 	}
-}( jQuery ));
+	
+	function getErrTitle(elm){
+		var label = elm.parent().find("label[for="+elm.attr("id")+"]");
+		if(isObj(label)&&label.html()!=undefined){
+			return label.html();
+		}
+		var elmAttrErr = elm.attr("data-err-title");
+		if(elmAttrErr.length>0){
+			return elmAttrErr;
+		}
+	}
+	
+	function getErrMessage(elm){
+		if(elm.attr("data-err-msg")){
+			return elm.attr("data-err-msg");
+		}
+		return "value required";
+	}
+	
+	function reset(e){
+		event=e;
+		self.errorMessages = [];
+		self.options.errorDisplayElm.empty();
+		self.options.errorDisplayElm.hide();
+	}
+	
+	function validate(e){
+		event=e;
+		var elm = $(self.element);
+		self.errorMessages = [];
+		elm.find(':invalid').each(function(index, node){
+			if($(this).is("input")){
+				self.err(getErrTitle($(this)),getErrMessage($(this)));
+			}
+		});
+		
+		elm.find("[data-validate]").each(function(){
+			switch($(this).attr("data-validate").toLowerCase()){
+				case "checkboxgroup":
+				case "checkbox_group":
+				case "checkbox-group":
+					var count=0;
+					var checkboxes = $(this).find("input[type=checkbox]");
+					var passed=false;
+					$(this).find("input[type=checkbox]").each(function(){
+						if($(this).is(':checked')){
+							count++;
+						}
+					});
+					if($(this).attr("data-min-select")){
+						if(count<$(this).attr("data-min-select")){
+							self.err(getErrTitle($(this)),"min of "+$(this).attr("data-min-select")+" selections must be made");
+						}
+					}
+					if($(this).attr("data-max-select")){
+						if(count>$(this).attr("data-max-select")){
+							self.err(getErrTitle($(this)),"max of "+$(this).attr("data-max-select")+" selections can be made");
+						}
+					}
+					
+				break;
+			}
+		
+		});
+		self.options.onValidate.call(self);
+		if(self.errorMessages.length>0){
+			stopSubmit(self);
+			self.options.onErrorsFound.call(self);
+		}
+	}
+	
+	function isObj(t){
+		return typeof(t)!==undefined;
+	}
+	
+})( jQuery, window, document );
