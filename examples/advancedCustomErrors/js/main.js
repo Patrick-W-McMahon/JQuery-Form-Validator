4d608845f8c59d2f12dd3e172a58e0a8109e8417
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-17 15:39:59 -0400

    Update main.js
    remove error class prior to adding them so that should a field be valid on 2nd submit it will show that the field is valid.

--- examples/advancedCustomErrors/js/main.js
+++ examples/advancedCustomErrors/js/main.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 	$("#testForm").validate({
 		onValidate:function(){
+			$(this.element).find(".error").removeClass("error");
 			var myErrors = this.getErrors()||[];
 			for(var x=0;x<myErrors.length;x++){
 				myErrors[x].field.addClass("error");
