8c82f1f8deac45ae3c8d96518cda192a5e4137d0
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-17 16:27:58 -0400

    Update main.js

--- examples/customError/js/main.js
+++ examples/customError/js/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 	$("#testForm").validate({
 		onValidate:function(){
-			var myErrors = this.getErrors()||[];
+			var myErrors = this.getErrors();
 			for(var x=0;x<myErrors.length;x++){
 				var err = myErrors[x];
 				console.log("Error "+x+": Title: "+err.title+", Message: "+err.msg+".");
