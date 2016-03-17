9dcded525af0925c61aac46e3447ac8a91c927f1
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-17 16:26:38 -0400

    Update main.js

--- examples/advancedCustomErrors/js/main.js
+++ examples/advancedCustomErrors/js/main.js
@@ -2,13 +2,13 @@ $( document ).ready(function() {
 	$("#testForm").validate({
 		onValidate:function(){
 			$(this.element).find(".error").removeClass("error");
-			var myErrors = this.getErrors()||[];
+			var myErrors = this.getErrors();
 			for(var x=0;x<myErrors.length;x++){
 				myErrors[x].field.addClass("error");
 			}
 		},
 		onReset:function(){
-			var myErrors = this.getErrors()||[];
+			var myErrors = this.getErrors();
 			for(var x=0;x<myErrors.length;x++){
 				myErrors[x].field.removeClass("error");
 			}
@@ -16,7 +16,7 @@ $( document ).ready(function() {
 		onDisplayErrors:function(){
 			var ede = this.options.errorDisplayElm;
 			if(typeof(ede)!=undefined){
-				var myErrors = this.getErrors()||[];
+				var myErrors = this.getErrors();
 				ede.empty();
 				for(var x=0;x<myErrors.length;x++){
 					ede.append($("<div><h2>"+myErrors[x].title+"</h2><p>"+myErrors[x].msg+"</p></div>"));
