5823b3cebb41522e582308ff9235b7ae5b3dd581
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-21 17:32:33 -0400

    Update JQuery-form-validator.js
    corrected many small errors that where preventing the new min and max check box group select error message attributes from working.

--- JQuery-form-validator.js
+++ JQuery-form-validator.js
@@ -74,10 +74,20 @@
 					case "checkbox-group":
 						var count=$(this).find("input[type=checkbox]:checked").length;
 						if($(this).attr("data-min-select")&&count<$(this).attr("data-min-select")){
-							self.err($(this),getErrTitle($(this)),"min of "+$(this).attr("data-min-select")+" selections must be made");
+							var msg = peramSet([
+								$(this).attr("min-select-err"),
+								$(this).attr("min_select_err"),
+								$(this).attr("minSelectErr"),
+							],"min of "+$(this).attr("data-min-select")+" selections must be made");
+							self.err($(this),getErrTitle($(this)),msg);
 						}
 						if($(this).attr("data-max-select")&&count>$(this).attr("data-max-select")){
-							self.err($(this),getErrTitle($(this)),"max of "+$(this).attr("data-max-select")+" selections can be made");
+							var msg = peramSet([
+								$(this).attr("min-select-err"),
+								$(this).attr("min_select_err"),
+								$(this).attr("minSelectErr"),
+							],"max of "+$(this).attr("data-max-select")+" selections can be made");
+							self.err($(this),getErrTitle($(this)),msg);
 						}
 					break;
 				}
@@ -139,5 +149,5 @@
 		}
 	}
 	
-	function nUo(t){return typeof(t)!==undefined;}	
+	function nUo(t){return typeof(t)!=="undefined";}	
 })(jQuery,window,document);
